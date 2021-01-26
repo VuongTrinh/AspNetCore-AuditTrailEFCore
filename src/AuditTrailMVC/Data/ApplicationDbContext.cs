3b690cbffa3caf1554076a966785a7f0fc9106a9
VuongTrinh <vuongtrinh.dev@gmail.com>
2021-01-26 15:13:23 +0700

    Add Audit Trail using EF Core.

---
+++ src/AuditTrailMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AuditTrailMVC.Data
+{
+    public class ApplicationDbContext : AuditableIdentityContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<AuditTrailMVC.Models.Product> Product { get; set; }
+    }
+}
